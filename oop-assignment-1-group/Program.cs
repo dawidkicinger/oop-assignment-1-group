d4021c9c8e34aac78e010b0c24750818b5d4b758
Dawid Piotr Kicinger <19701329@students.lincoln.ac.uk>
2021-03-03 18:19:42 +0000

    test1

--- oop-assignment-1-group/Program.cs
+++ oop-assignment-1-group/Program.cs
@@ -7,6 +7,7 @@ namespace oop_assignment_1_group
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hi");
         }
     }
 }
