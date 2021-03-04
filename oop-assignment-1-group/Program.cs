67dc95e7b9580aed956b704ec3e8b8611ab55df9
Dawid Piotr Kicinger <19701329@students.lincoln.ac.uk>
2021-03-04 19:56:45 +0000

    xd

--- oop-assignment-1-group/Program.cs
+++ oop-assignment-1-group/Program.cs
@@ -41,7 +41,7 @@ namespace oop_assignment_1_group
 
             int[] array1 = { ld, lab, con, gr, bp , ukp, cuk, inet, ind };
 
-            // Find maximum number.xd
+            // Find maximum number.
           
 
             if (array1.Max() == bp)
